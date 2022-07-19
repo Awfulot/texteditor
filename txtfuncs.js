77d66e54faa78ecdb0598bcd0afde599612d03ee
Awfulot <Mr.Snisken1@gmail.com>
2022-07-19 11:27:12 -0400

    Made bold and italic buttons functional

--- txtfuncs.js
+++ txtfuncs.js
@@ -1,1 +1,35 @@
 //test
+
+function makeBold(){
+    document.getElementById("userText").style.fontWeight = "bold";
+}
+
+function makeItalic(){
+    document.getElementById("userText").style.fontStyle = "italic";
+}
+
+function leftAlign(){
+    document.getElementById("userText").style.text.leftAlign;
+}
+
+function rightAlign(){
+    document.getElementById("userText").innerHTML.text.Italics();
+}
+
+
+function centerAlign(){
+    document.getElementById("userText").innerHTML.text.Italics();
+    
+}
+
+
+function upperCase(){
+    document.getElementById("userText").innerHTML.text.Italics();
+}
+
+function lowerCase(){
+    document.getElementById("userText").innerHTML.text.Italics();
+}
+function clearText(){
+    document.getElementById("userText").innerHTML.text.Italics();
+}
