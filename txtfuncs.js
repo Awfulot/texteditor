1202e12019411600c592cd92dd74d5a0a22ecc77
Awfulot <Mr.Snisken1@gmail.com>
2022-07-19 12:07:21 -0400

    Added functionality to all buttons, removed extra input

--- txtfuncs.js
+++ txtfuncs.js
@@ -1,35 +1,38 @@
 //test
 
 function makeBold(){
-    document.getElementById("userText").style.fontWeight = "bold";
+    document.getElementById("userText").style.fontWeight="bold";
 }
 
 function makeItalic(){
-    document.getElementById("userText").style.fontStyle = "italic";
+    document.getElementById("userText").style.fontStyle="italic";
 }
 
 function leftAlign(){
-    document.getElementById("userText").style.text.leftAlign;
+    document.getElementById("userText").style.textAlign="left";
 }
 
 function rightAlign(){
-    document.getElementById("userText").innerHTML.text.Italics();
+    document.getElementById("userText").style.textAlign="right";
 }
 
 
 function centerAlign(){
-    document.getElementById("userText").innerHTML.text.Italics();
+    document.getElementById("userText").style.textAlign="center";
     
 }
 
 
 function upperCase(){
-    document.getElementById("userText").innerHTML.text.Italics();
+    document.getElementById("userText").style.textTransform = "uppercase";
 }
 
 function lowerCase(){
-    document.getElementById("userText").innerHTML.text.Italics();
+    document.getElementById("userText").style.textTransform = "lowercase";
+}
+function capitalize(){
+    document.getElementById("userText").style.textTransform = "capitalize";
 }
 function clearText(){
-    document.getElementById("userText").innerHTML.text.Italics();
+    document.getElementById("userText").style.textDecoration.none;
 }
